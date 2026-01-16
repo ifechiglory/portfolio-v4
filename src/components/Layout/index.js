aae0352495f6d1cecb7a8b5830c5eda4604b6ee8
Ifechukwu Edet <ifechiglory@gmail.com>
2026-01-16 18:06:38 +0100

    update favicon

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -22,6 +22,24 @@ const Layout = ({ children }) => {
           name="description"
           content="Frontend Developer specializing in React & Gatsby"
         />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
       </Helmet>
 
       <div className="min-h-screen pt-10 bg-navy text-light-slate">
